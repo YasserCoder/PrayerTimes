30fa155c13542a1c5d449fa6539eecfa4851d310
yasser <yasser.laiss01@gmail.com>
2024-02-12 20:06:17 +0100

    uploading project

---
+++ app.js
@@ -0,0 +1,163 @@
+let countrylist = document.querySelector("#country-select");
+let citylist = document.querySelector("#city-select");
+let btn = document.querySelector(".btn");
+let datepicker = document.querySelector("#datePicker");
+
+function getcountry() {
+    return new Promise((resolve, reject) => {
+        fetch("https://countriesnow.space/api/v0.1/countries/iso")
+            .then((response) => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    reject("error with users request");
+                }
+            })
+            .then((countries) => {
+                for (let country of countries.data) {
+                    let option = document.createElement("option");
+                    option.value = country.name;
+                    option.textContent = country.name;
+                    countrylist.appendChild(option);
+                }
+                let countrysl;
+                if (localStorage.length !== 0) {
+                    countrysl = localStorage.getItem("country");
+                } else {
+                    countrysl = "Algeria";
+                }
+                document
+                    .querySelector(
+                        "#country-select option[value='" + countrysl + "']"
+                    )
+                    .setAttribute("selected", "");
+
+                resolve(countrysl);
+            });
+    });
+}
+
+function getcities(countrysl) {
+    return new Promise((resolve, reject) => {
+        fetch("https://countriesnow.space/api/v0.1/countries/states", {
+            method: "POST",
+            body: JSON.stringify({ country: countrysl }),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+            .then((response) => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    reject("error with users request");
+                }
+            })
+            .then((states) => {
+                citylist.innerHTML = "";
+                for (let state of states.data.states) {
+                    let option = document.createElement("option");
+                    option.value = state.name;
+                    option.textContent = state.name;
+                    citylist.appendChild(option);
+                }
+                let selectedIndex = citylist.selectedIndex;
+
+                let citysl = citylist.options[selectedIndex].value;
+                resolve(citysl);
+
+                btn.removeAttribute("disabled");
+            });
+    });
+}
+
+function getprayers(country, city, date) {
+    let link = "";
+    if (date === "") {
+        link =
+            "http://api.aladhan.com/v1/timingsByCity?city=" +
+            city +
+            "&country=" +
+            country;
+    } else {
+        link =
+            "http://api.aladhan.com/v1/timingsByCity/" +
+            date +
+            "?city=" +
+            city +
+            "&country=" +
+            country;
+    }
+
+    fetch(link)
+        .then((response) => {
+            if (response.ok) {
+                return response.json();
+            }
+        })
+        .then((prayers) => {
+            let obj = prayers.data.timings;
+            let cells = document.querySelectorAll(".cell .prayer");
+
+            for (let i = 0; i < cells.length; i++) {
+                let prayername = cells[i].getAttribute("id");
+                cells[i].nextElementSibling.textContent = obj[prayername];
+            }
+            btn.setAttribute("disabled", "");
+        })
+        .catch(() => {
+            alert("cannot connect to API");
+        });
+}
+
+countrylist.addEventListener("change", (e) => {
+    getcities(e.target.value);
+});
+
+datepicker.addEventListener("change", (e) => {
+    setTimeout(() => {
+        btn.removeAttribute("disabled");
+    }, 3000);
+});
+citylist.addEventListener("change", (e) => {
+    btn.removeAttribute("disabled");
+});
+
+btn.addEventListener("click", (e) => {
+    let selectedIndex = countrylist.selectedIndex;
+
+    let country = countrylist.options[selectedIndex].value;
+
+    selectedIndex = citylist.selectedIndex;
+    let city = citylist.options[selectedIndex].value;
+
+    let chosendate = document.querySelector("#datePicker").value;
+
+    localStorage.setItem("country", country);
+    localStorage.setItem("state", city);
+
+    getprayers(country, city, chosendate);
+});
+
+let today = new Date();
+datepicker.value =
+    today.getFullYear() +
+    "-" +
+    (today.getMonth() + 1).toString().padStart(2, "0") +
+    "-" +
+    today.getDate().toString().padStart(2, "0");
+
+getcountry().then((countrysl) => {
+    getcities(countrysl).then((ctysl) => {
+        let state;
+        if (localStorage.length !== 0) {
+            state = localStorage.getItem("state");
+            document
+                .querySelector("#city-select option[value='" + state + "']")
+                .setAttribute("selected", "");
+        } else {
+            state = ctysl;
+        }
+        getprayers(countrysl, state, "");
+    });
+});
